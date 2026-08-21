c6b9d3e361e6aa98702e634b392df14d4a5e13d3
Zuhairi1988 <118650027+Zuhairi1988@users.noreply.github.com>
2026-08-21 22:16:20 +0800

    Simplify Quran reader filter labels

--- sw-register.js
+++ sw-register.js
@@ -2,7 +2,7 @@
   if (!('serviceWorker' in navigator)) return;
 
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js?v=20260821-8', {
+    navigator.serviceWorker.register('./service-worker.js?v=20260821-9', {
       scope: './',
       updateViaCache: 'none'
     }).catch(() => {
